2622f1add57a6e1e469296f11ce6956074c1610b
Nickolay <kola2003.ryabof@yandex.ru>
2023-12-04 22:53:29 +0500

    lab 4 complete

---
+++ Enterprise Management/Logger/Logger.cs
@@ -0,0 +1,18 @@
+﻿namespace Enterprise_Management.Logger;
+
+public class Logger
+{
+    public delegate void EventHandler(string message); // делегат(шаблон того, какие методы мы сможем записать в наш Event)
+
+    public event EventHandler? LogEvent; // event, куда записаны методы логирования
+    
+    public void AddHandler(EventHandler handler)
+    {
+        // Записываем все передаенные метожы логгирования в переменную
+        LogEvent += handler;
+    }
+    public void LogMessage(string message)
+    {
+        LogEvent?.Invoke(message);
+    }
+}
