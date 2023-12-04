fb9d1fa09af1913bab275c9447e2fede0c8e6996
Nickolay <kola2003.ryabof@yandex.ru>
2023-12-04 17:41:45 +0500

    lab 2 complete

---
+++ Enterprise Management/Collection/IEmployeeCollection.cs
@@ -0,0 +1,9 @@
+﻿using Enterprise_Management.Models.Employees;
+
+namespace Enterprise_Management.Collection;
+
+public interface IEmployeeCollection<T> : IEnumerable<T> where T : Employee
+{
+    public int Count { get; }
+    public void Add(T item);
+}
--- Enterprise Management/Models/Factory.cs
+++ Enterprise Management/Models/Factory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using Enterprise_Management.Collection;
 using Enterprise_Management.Models.Employees;
 using Enterprise_Management.Models.Products;
 
@@ -6,8 +7,27 @@ namespace Enterprise_Management.Models;
 
 public class Factory
 {
+    // ============================================================
     // Список сотрудников
-    private List<Employee> _employees = new List<Employee>(); 
+    // Переделали так что каждый тип сотрудника записан в отдельном списке
+    private IEmployeeCollection<Manager> _managers = new EmployeeCollection<Manager>(); 
+    private IEmployeeCollection<FactoryWorker> _factoryWorkers = new EmployeeCollection<FactoryWorker>();
+    private IEmployeeCollection<Salesperson> _salesPersons = new EmployeeCollection<Salesperson>();
+    
+    // Свойство, которое возвращает полный список сотрудников
+    private List<Employee> _employees
+    {
+        get
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.AddRange(_managers);
+            employees.AddRange(_factoryWorkers);
+            employees.AddRange(_salesPersons);
+            return employees;
+        }
+    }
+
+    // ============================================================
     // Список товариов
     private List<Product> _productsList = new List<Product>();
     public List<Product> Products => new(_productsList); // свойство, возвращает копию списка продуктов во внешний истточник
@@ -54,11 +74,24 @@ public class Factory
         // Новый товар
         _productsList.Add(product);
     }
-
-    public void AddNewEmployee(Employee employee)
+    // Абстрактный метод добавления сотрудника
+    public void AddNewEmployee<T>(T employee) where T : Employee
     {
         // Новый сотрудник
-        _employees.Add(employee);
+        // Проверяем тип сотрудника
+        // В зависимости от типа добавляем в поределнный список
+        switch (employee)
+        {
+            case Manager manager:
+                _managers.Add(manager);
+                break;
+            case FactoryWorker factoryWorker:
+                _factoryWorkers.Add(factoryWorker);
+                break;
+            case Salesperson salesperson:
+                _salesPersons.Add(salesperson);
+                break;
+        }
     }
     // ====================================================
     
